212230a7e5ece12b1ed16d1cdd129f18868c8f69
ezgideren <ezgi_deren@yahoo.com>
2024-06-22 00:15:57 +0200

    minimal changes made on login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,14 +5,14 @@ const LoginPage = () => {
   return (
     <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
       {/*BOX*/}
-      <div className="h-full shadow-2xl">
+      <div className="h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2 ">
         {/*IMAGE CONTAINER*/}
-        <div className="relative">
-          <Image src="/login.png" alt="" fill />
+        <div className="relative h-1/2 w-full md:h-full md:w-1/2">
+          <Image src="/login.png" alt="" fill className="object-scale-down" />
         </div>
         {/*FORM CONTAINER*/}
-        <div className="">
-          <h1>Welcome</h1>
+        <div className="p-10 flex flex-col gap-8 md:h-full md:w-1/2">
+          <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
           <p>Log in to your acoount or create a new one</p>
           <button className="flex gap-4 p-4 ring-1 rounded-md">
             <Image src="/google.webp" alt="" width={20} height={20} className="object-contain"/>
--- components/Slider.tsx
+++ components/Slider.tsx
@@ -5,15 +5,19 @@ import Image from "next/image";
 const Slider = () => {
     return (
         <div className="flex flex-col h-[calc(100vh-5rem)] md:h-[calc(100vh-9rem)] lg:flex-row">
-            {/* TEXT CONTAINER */}
-            <div className="h-1/2 flex items-center justify-center flex-col gap-8 text-[#333230] font-bold lg:h-full lg:w-1/2">
-                <h1 className="text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl">test</h1>
-                <button className="bg-[#333230] text-white hover:bg-[#e5ccb0] py-4 px-8">Reserve Now</button>
-            </div>
             {/* IMAGE CONTAINER */}
             <div className="w-full h-1/2 relative lg:h-full lg:w-1/2">
-                <Image src="/slider1.jpeg" alt="" fill className="object-cover" />
+                <Image src="/slide3.jpeg" alt="" fill className="object-contain" />
             </div>
+
+            {/* TEXT CONTAINER */}
+            <div className="h-1/2 flex items-center justify-center flex-col gap-8 text-[#333230] font-bold lg:h-full lg:w-1/2">
+                <div className="w-1/2 h-1/2 relative p-4">
+                <Image src="/logo.webp" alt="" fill className="object-scale-down" />
+                </div>
+                <button className="bg-[#333230] text-white hover:bg-[#e5ccb0] py-4 px-8">Reserve Now</button>
+            </div>
+            
         </div>
     )
  }
